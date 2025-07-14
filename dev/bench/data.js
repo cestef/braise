d6ef0d3e1cf668507c5a38104bcd359bf55e30d2
github-action-benchmark <github@users.noreply.github.com>
2025-07-14 12:42:18 +0000

    add Benchmark (cargo) benchmark result for f10db1c516f3d1885f5917f13ad9dbbed764c0ea

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752493415092,
+  "lastUpdate": 1752496938293,
   "repoUrl": "https://github.com/cestef/braise",
   "entries": {
     "Benchmark": [
@@ -16,89 +16,89 @@ window.BENCHMARK_DATA = {
             "username": "cestef"
           },
           "distinct": true,
-          "id": "baa5d28d86df36460b2bd37fa97505262cff168e",
-          "message": "fix: remove useless lib.rs",
-          "timestamp": "2025-07-14T13:39:56+02:00",
-          "tree_id": "7c0be87d5644509b970c703514ca631c13ba73ac",
-          "url": "https://github.com/cestef/braise/commit/baa5d28d86df36460b2bd37fa97505262cff168e"
+          "id": "f10db1c516f3d1885f5917f13ad9dbbed764c0ea",
+          "message": "feat: deploy docs",
+          "timestamp": "2025-07-14T14:38:56+02:00",
+          "tree_id": "ec1c47fb48b9983de70b9181bcbb275bbc629b84",
+          "url": "https://github.com/cestef/braise/commit/f10db1c516f3d1885f5917f13ad9dbbed764c0ea"
         },
-        "date": 1752493414840,
+        "date": 1752496937656,
         "tool": "cargo",
         "benches": [
           {
             "name": "lexer/tokenize/small",
-            "value": 784,
-            "range": "± 2",
+            "value": 791,
+            "range": "± 24",
             "unit": "ns/iter"
           },
           {
             "name": "lexer/tokenize/medium",
-            "value": 3483,
-            "range": "± 11",
+            "value": 3458,
+            "range": "± 14",
             "unit": "ns/iter"
           },
           {
             "name": "lexer/tokenize/large",
-            "value": 8587,
-            "range": "± 74",
+            "value": 8446,
+            "range": "± 333",
             "unit": "ns/iter"
           },
           {
             "name": "parser/parse/small",
-            "value": 13391,
-            "range": "± 50",
+            "value": 13647,
+            "range": "± 320",
             "unit": "ns/iter"
           },
           {
             "name": "parser/parse/medium",
-            "value": 52769,
-            "range": "± 529",
+            "value": 52485,
+            "range": "± 147",
             "unit": "ns/iter"
           },
           {
             "name": "parser/parse/large",
-            "value": 79021,
-            "range": "± 1240",
+            "value": 81279,
+            "range": "± 157",
             "unit": "ns/iter"
           },
           {
             "name": "pipeline/full/small",
-            "value": 16122,
-            "range": "± 370",
+            "value": 17966,
+            "range": "± 510",
             "unit": "ns/iter"
           },
           {
             "name": "pipeline/full/medium",
-            "value": 60839,
-            "range": "± 948",
+            "value": 61757,
+            "range": "± 3470",
             "unit": "ns/iter"
           },
           {
             "name": "pipeline/full/large",
-            "value": 92716,
-            "range": "± 590",
+            "value": 94365,
+            "range": "± 798",
             "unit": "ns/iter"
           },
           {
             "name": "runtime/execute/small",
-            "value": 1512,
-            "range": "± 4",
+            "value": 1519,
+            "range": "± 17",
             "unit": "ns/iter"
           },
           {
             "name": "runtime/execute/medium",
-            "value": 5077,
-            "range": "± 19",
+            "value": 4938,
+            "range": "± 21",
             "unit": "ns/iter"
           },
           {
             "name": "runtime/execute/large",
-            "value": 8297,
-            "range": "± 17",
+            "value": 7639,
+            "range": "± 38",
             "unit": "ns/iter"
           }
         ]
       }
     ]
   }
-};
+}
