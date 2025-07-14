0430e89edf2e244d7f01e1b0ddf1c2f351873c56
github-action-benchmark <github@users.noreply.github.com>
2025-07-14 14:00:40 +0000

    add Benchmark (cargo) benchmark result for 0084c0c662c1881adbc3859bb6a2444d450e06ae

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752496938293,
+  "lastUpdate": 1752501640197,
   "repoUrl": "https://github.com/cestef/braise",
   "entries": {
     "Benchmark": [
@@ -95,6 +95,102 @@ window.BENCHMARK_DATA = {
             "name": "runtime/execute/large",
             "value": 7639,
             "range": "± 38",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "root@cstef.dev",
+            "name": "cstef",
+            "username": "cestef"
+          },
+          "committer": {
+            "email": "root@cstef.dev",
+            "name": "cstef",
+            "username": "cestef"
+          },
+          "distinct": true,
+          "id": "0084c0c662c1881adbc3859bb6a2444d450e06ae",
+          "message": "feat: add new banner images and enhance homepage layout",
+          "timestamp": "2025-07-14T15:57:15+02:00",
+          "tree_id": "4394b37116a26620e7e8cfc9ac479c89442af117",
+          "url": "https://github.com/cestef/braise/commit/0084c0c662c1881adbc3859bb6a2444d450e06ae"
+        },
+        "date": 1752501639277,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "lexer/tokenize/small",
+            "value": 780,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "lexer/tokenize/medium",
+            "value": 3291,
+            "range": "± 16",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "lexer/tokenize/large",
+            "value": 8499,
+            "range": "± 46",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "parser/parse/small",
+            "value": 13367,
+            "range": "± 87",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "parser/parse/medium",
+            "value": 53634,
+            "range": "± 437",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "parser/parse/large",
+            "value": 77702,
+            "range": "± 601",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pipeline/full/small",
+            "value": 17085,
+            "range": "± 257",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pipeline/full/medium",
+            "value": 62238,
+            "range": "± 701",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pipeline/full/large",
+            "value": 93899,
+            "range": "± 384",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "runtime/execute/small",
+            "value": 1463,
+            "range": "± 45",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "runtime/execute/medium",
+            "value": 4918,
+            "range": "± 13",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "runtime/execute/large",
+            "value": 7566,
+            "range": "± 13",
             "unit": "ns/iter"
           }
         ]
